a88e286216634b2bbc9599f95af7efe2b05ed2dc
SouravShrestha <shrestha.sourav30@gmail.com>
2018-01-15 02:17:41 +0530

    .

--- Assets/Rotator.cs
+++ Assets/Rotator.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Rotator : MonoBehaviour {
 
-    public float speed = 100f; 
+    public float speed = 100f;
 	
 	void Update () {
 		transform.Rotate (0f, 0f, speed * Time.deltaTime);
